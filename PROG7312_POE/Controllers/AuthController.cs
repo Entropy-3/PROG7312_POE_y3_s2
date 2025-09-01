7449020700507597d0e6563bff616b26400adfb0
Nathan Teixeira <st10249266@vcconnect.edu.za>
2025-09-01 13:53:27 +0200

    -init commit -signup UI -login UI -nav bar UI

---
+++ PROG7312_POE/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROG7312_POE.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
