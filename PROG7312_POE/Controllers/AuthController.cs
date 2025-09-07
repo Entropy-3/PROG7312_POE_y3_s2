c5744327c57e1347f3258e476ccea63a897ea595
Entropy-3 <Nathanteixeira981@gmail.com>
2025-09-07 18:49:42 +0200

    added migrations as well as a signup method

--- PROG7312_POE/Controllers/AuthController.cs
+++ PROG7312_POE/Controllers/AuthController.cs
@@ -1,12 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PROG7312_POE.Models;
 
 namespace PROG7312_POE.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly AppDbContext _context;
+
         public IActionResult Login()
         {
             return View();
+        }
+        
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+        //method that creates user in the database
+        [HttpPost]
+        public IActionResult SignUp(userTBL user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                user.Email = user.Email.ToLower();
+                //checks to see if the email is already taken
+                if (_context.Users.Any(u => u.Email == user.Email))
+                {
+                    ModelState.AddModelError("Email", "Email is already taken.");
+                    return View(user);
+                }
+                //hashes password for security
+                user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            //else statement for debugging purposes
+            else
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+                return View(user);
+            }
+
         }
 
         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
@@ -17,10 +67,6 @@ namespace PROG7312_POE.Controllers
             HttpContext.Session.Clear();
             return RedirectToAction("Index", "Home");
         }
-
-        public IActionResult SignUp()
-        {
-            return View();
-        }
+        
     }
 }
