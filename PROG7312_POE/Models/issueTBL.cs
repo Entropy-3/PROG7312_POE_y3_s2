2caa5eea703ded430c25c58d066a1759b1c03706
Entropy-3 <Nathanteixeira981@gmail.com>
2025-09-08 14:02:41 +0200

    Refactor issue form and add database migration classes
    Updated `AddIssue.cshtml` to use Razor comments for better readability. Added migration class `addedIssueTBL` with placeholders for future schema changes. Generated `20250908120225_addedIssueTBL.Designer.cs` to define the `userTBL` model. Created `issueTBL.cs` to represent the issue entity with necessary properties and relationships.

---
+++ PROG7312_POE/Models/issueTBL.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PROG7312_POE.Models
+{
+    public class issueTBL
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IssueID { get; set; }
+
+        //Ai assisted with foreign key
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+        public userTBL? User { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public byte[]? DocumentData { get; set; }
+    }
+}
