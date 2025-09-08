af0ef9b1a93e526e0ad2fae63d4064e7ee197731
Entropy-3 <Nathanteixeira981@gmail.com>
2025-09-08 13:51:48 +0200

    Update routing and enhance UI for various pages
    - Updated links in `Index.cshtml` to new action methods in `Events` and `Services` controllers.
    - Revamped `Privacy.cshtml` with a new layout, header, subheader, and improved CSS styling.
    - Removed commented-out code in `ViewIssues.cshtml`.
    - Created `EventsController` and `ServicesController` with methods returning views.
    - Added `Events.cshtml` and `Services.cshtml` with a consistent structure and styling, indicating pages are under construction.
    - Overall improvements to user interface and application organization.

---
+++ PROG7312_POE/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROG7312_POE.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Events()
+        {
+            return View();
+        }
+    }
+}
---
+++ PROG7312_POE/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROG7312_POE.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
+}
