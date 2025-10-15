dd109e77fd8802fbffb4793751ba7be518401b8e
Entropy-3 <Nathanteixeira981@gmail.com>
2025-10-15 12:08:28 +0200

    Enhance event model and update related files
    Updated `eventTBL` class with data annotations for validation and database generation. Added a commented-out line for potential `IEventService` registration in `Program.cs`. Removed a comment in `AddEvent.cshtml` regarding date input. Cleaned up CSS styles in `Events.cshtml` and removed the "Actions" column from the event table. Created a new migration file `20251015100646_stuff.cs` with empty methods, along with its designer file, to define `issueTBL` and `userTBL` models. Noted changes in the `localdb.sqlite` binary file.

--- PROG7312_POE/Models/eventTBL.cs
+++ PROG7312_POE/Models/eventTBL.cs
@@ -1,12 +1,27 @@
-﻿namespace PROG7312_POE.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PROG7312_POE.Models
 {
     public class eventTBL
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int EventID { get; set; }
+
+        [Required]
         public string EventName { get; set; }
+
+        [Required]
         public string EventCategory { get; set; }
+
+        [Required]
         public DateTime EventDate { get; set; }
+
+        [Required]
         public string Location { get; set; }
+
+        [Required]
         public string Description { get; set; }
     }
 }
